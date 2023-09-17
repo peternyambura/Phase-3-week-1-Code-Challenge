4911b729acfd39f4e5e6f0670e494e2892db5283
= <peterndirangu0005@gmail.com>
2023-09-17 23:32:27 +0300

    Done

---
+++ remove_duplicates.py
@@ -0,0 +1,10 @@
+def remove_duplicates(sequence):
+    seen = set()
+    result = []
+
+    for item in sequence:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+
+    return result
